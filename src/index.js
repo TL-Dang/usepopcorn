b19f3ff194bb70788c5a5d6aec93004b1ec4b4bf
Tommy Dang <ledangt310@live.com>
2023-07-03 16:42:52 -0700

    add StarRating component with logic and css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import StarRating from './components/StarRating/StarRating.component';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
